669d0af3bca1b97d41d0947fbd5af8f9dbf11d9a
alhe9379 <alhe9379@colorado.edu>
2021-11-15 11:51:10 -0700

    Implemented stupid way of having slider DOM element emit nothing useful. Pushing this just to save it in case.

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {RoomServiceService} from "../services/room-service.service";
 import {Card} from "../interfaces/card";
+import { fromEvent } from 'rxjs';
+// https://www.tektutorialshub.com/angular/elementref-in-angular/ <-- for accessing DOM (in unsafe way)
+// https://www.tektutorialshub.com/angular/create-observable-from-event-using-fromevent-in-angular/ <-- for making DOM element observable
 
 @Component({
   selector: 'app-playground',
@@ -9,13 +12,20 @@ import {Card} from "../interfaces/card";
 })
 export class PlaygroundComponent implements OnInit {
   localCard: Card[];
+  @ViewChild('slider') slider: ElementRef;
 
   constructor(readonly rs: RoomServiceService) {
     this.localCard = rs.getCards();
   }
 
   ngOnInit(): void {
+  }
 
+  // https://stackoverflow.com/questions/42454740/angular-2-subscribing-to-observable-fromevent-error-invalid-event-target
+  ngAfterViewInit() {
+    let slider$ = fromEvent(this.slider.nativeElement, 'mouseup')
+      .subscribe(res => console.log(res));
   }
 
 }
+
